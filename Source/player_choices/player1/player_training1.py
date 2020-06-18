d0005f5c7e0a8986bb350ac0235eff9327b2a00d
Tyson Lemire <tyson.lemire@7shifts.com>
2020-06-18 07:04:57 -0600

    after training1

--- Source/player_choices/player1/player_training1.py
+++ Source/player_choices/player1/player_training1.py
@@ -15,7 +15,7 @@ def player1_handle_outcome(game_info):
 def player1choice(gamestate):
     prediction = t.predict(gamestate)
     if 'check' not in gamestate['betting_info']['betting_options']:
-        if (prediction.max() > 0.50):
+        if (prediction.max() > 0.75):
             return {'choice': 'bet', 'amount':25}
         return {'choice': 'call'}
     return {'choice': 'check'}
