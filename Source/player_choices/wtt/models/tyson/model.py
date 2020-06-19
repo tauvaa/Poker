75588fecad6d040d8204a7b2ad3293cbecfc9242
Tyson Lemire <tyson.lemire@7shifts.com>
2020-06-19 11:15:42 -0600

    add tyson model

---
+++ Source/player_choices/wtt/models/tyson/model.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+
+import pickle
+import numpy as np
+from itertools import repeat
+import datetime
+
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.callbacks import TensorBoard
+import joblib
+from sklearn.model_selection import train_test_split
+
+from os import listdir
+from os.path import dirname, join, exists
+import pprint
+
+MODEL_NAME = 'model.pkl'
+LOG = True
+
+
+def init():
+    # define the keras model
+    model = Sequential()
+    model.add(Dense(32, input_dim=52, activation='relu'))
+    model.add(Dense(16, activation='relu'))
+    model.add(Dense(8, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    model.summary()
+    compile(model)
+    save(model)
+    return model
+
+
+def compile(model):
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+
+def save(model):
+    joblib.dump(model, join(dirname(__file__), MODEL_NAME))
+
+
+def load():
+    model_file = join(dirname(__file__), MODEL_NAME)
+    if exists(model_file):
+        model = joblib.load(join(dirname(__file__), MODEL_NAME))
+        compile(model)
+    else:
+        model = init()
+    model.summary()
+    return model
+
+
+def save_data(game_info):
+    next_data_file = listdir(join(dirname(__file__), 'data_store'))
+    if len(next_data_file) == 0:
+        next_data_file = 0
+    else:
+        next_data_file = [int(n) for n in next_data_file]
+        next_data_file = max(next_data_file) + 1
+    with open(join(dirname(__file__), 'data_store', str(next_data_file)),
+              'ab+') as f:
+        f.write(pickle.dumps(game_info))
+
+
+def load_data():
+    all_data = []
+    for x in listdir(join(dirname(__file__), 'data_store')):
+        to_ap = join(dirname(__file__), 'data_store', x)
+        with open(to_ap, 'rb') as f:
+            all_data.append(pickle.load(f))
+    return all_data
+
+
+def parse_data(game_info_array):
+    x_list = []
+    y_train = []
+    for k in game_info_array:
+        flop = np.array(k['community_cards']['flop']['hand_matrix']).flatten()
+        turn = np.array(k['community_cards']['turn']['hand_matrix']).flatten()
+        river = np.array(
+            k['community_cards']['river']['hand_matrix']).flatten()
+        player_end = np.array(
+            k['player_cards']['player 1']['hand_matrix'] if k['is_fold'] else
+            k['player_cards'][0]['player 1']['hand_matrix'],
+            dtype=bool).flatten()
+        player_turn = np.logical_xor(player_end, river)
+        player_flop = np.logical_xor(player_turn, turn)
+        player_pre_flop = np.logical_xor(player_flop, flop)
+
+        x_list.extend([player_end, player_turn, player_flop, player_pre_flop])
+        y_train.extend(repeat(1 if k['winner'] == 'player 1' else 0, 4))
+    return [np.array(x_list), y_train]
+
+
+def train(model, x_train, y_train):
+    log_dir = join(dirname(__file__), "logs/")
+    tensor_log = log_dir + "fit/{}" + datetime.datetime.now().strftime(
+        "%Y%m%d-%H%M%S")
+    tensorboard_callback = TensorBoard(log_dir=log_dir,
+                                       histogram_freq=1) if LOG else []
+    #fit it to the dataset
+    x_train, x_val, y_train, y_val = train_test_split(x_train,
+                                                      y_train,
+                                                      test_size=0.20,
+                                                      shuffle=True)
+    history = model.fit(
+        x_train,
+        y_train,
+        epochs=150,
+        batch_size=32,
+        validation_data=(x_val, y_val),
+        callbacks=[tensorboard_callback],
+    )
+    #evaluate
+    loss, accuracy = model.evaluate(x_train, y_train)
+    output = 'Accuracy: %.2f' % (accuracy *
+                                 100) + ', Loss: %.2f' % (loss * 100) + '\n'
+    if LOG:
+        with open(log_dir + 'log.txt', "a") as log:
+            log.write(output)
+            log.write(str(history.history))
+            log.write('\n--------------------\n')
+    print(output)
+    save(model)
+
+
+def predict(model, gamestate):
+    hand = np.array(gamestate['player_info']['hand']['hand_matrix'],
+                    dtype=bool).flatten()
+    return model.predict(np.array([hand]))
---
+++ Source/player_choices/wtt/models/tyson/player.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import random
+import numpy as np
+import json
+import pprint
+import pickle
+from os.path import join, dirname
+from os import listdir
+from Source.player_choices.examples.player_input import check_through
+import Source.player_choices.wtt.models.tyson.model as tyson_model
+
+model = tyson_model.load()
+realtime_data = []
+
+TRAINING = True
+HANDS_PLAYED_BATCH_SIZE = 10000
+REALTIME = False
+
+
+def train(gamestate):
+    if len(realtime_data) < HANDS_PLAYED_BATCH_SIZE * 4:
+        realtime_data.append(gamestate)
+    else:
+        [x_train, y_train] = tyson_model.parse_data(realtime_data)
+        tyson_model.train(model, x_train, y_train)
+        realtime_data.clear()
+
+
+def player1_handle_outcome(gamestate):
+    if TRAINING:
+        train(gamestate) if REALTIME else tyson_model.save_data(gamestate)
+    return
+
+
+def choose(gamestate):
+    prediction = tyson_model.predict(model, gamestate)
+    if 'check' not in gamestate['betting_info']['betting_options']:
+        if (prediction.max() > 0.75):
+            return {'choice': 'bet', 'amount': 25}
+        return {'choice': 'call'}
+    return {'choice': 'check'}
+
+
+def player1choice(gamestate):
+    return check_through(gamestate) if TRAINING else choose(gamestate)
